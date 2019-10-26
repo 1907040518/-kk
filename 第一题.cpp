e74db5a3c70bc4d23ede26bef1aad2e05df3fe19
837876909@qq.com <837876909@qq.com>
2019-10-26 08:46:07 +0800

    提交

---
+++ 第一题.cpp
@@ -0,0 +1,23 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b, c, m, n, d,e;
+//	scanf("%d%d", &a, &b);
+//	m = a;
+//	n = b;
+//	if (a < b)
+//	{
+//		d = a;
+//		a = b;
+//		b = d;
+//	}
+//	c = a % b;
+//	while (c != 0)
+//	{
+//		a = b;
+//		b = c;
+//		c = a % b;
+//	}
+//	e = (m * n) / b;
+//	printf("%d\n%d", b, e);
+//}
