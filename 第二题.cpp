023deafe7b8fb336a9b57da93f63640f34a48671
837876909@qq.com <837876909@qq.com>
2019-10-27 18:54:25 +0800

    ++

---
+++ 第二题.cpp
@@ -0,0 +1,21 @@
+//#include<stdio.h>
+//int factorial(int n)
+//{
+//	int result = 1;
+//	while (n != 0)
+//	{
+//		result *= n--;
+//	}
+//	printf("n=%d\n", n);
+//	return result;
+//}
+//int mian()
+//{
+//	int num1, num2, result;
+//	printf("input 2 numbers num1,num2=");
+//	scanf("%d,%d", &num1, &num2);
+//	result = factorial( num1) + factorial(num2);
+//	printf("num1!+num!=%d", result);
+//	printf("\n num1=%d,num2=%d\n", num1, num2);
+//	return 0;
+//}
